18d00e5128300483e21f409ad602e02cf7dd7bb9
Vinayak Mane <vinayakmane0987@gmail.com>
2022-11-17 08:30:23 +0530

    changed main to master

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/globals.css';
+import { Toaster }  from 'react-hot-toast';
+import type { AppProps } from 'next/app'
+import { Layout } from '../components';
+import {StateContext} from '../context/StateContext';
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <StateContext >
+    <Layout>
+      <Toaster />
+      <Component {...pageProps} />
+    </Layout>
+    </StateContext>
+  )
+}
